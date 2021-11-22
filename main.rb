b5c222c5d817f9cc378dd9dfbe45dc1dc2189ae7
Dragon Controller <gracefulfowler@gmail.com>
2021-11-21 22:33:13 -0800

    reflected POODR principles

--- main.rb
+++ main.rb
@@ -1,13 +1,14 @@
 !# /usr/bin/env ruby
 
 require_relative 'bike'
+require_relative 'luggage'
 require_relative 'rental'
 
 items = [:apple, :water, :protein_bar]
 
-bike = Bike.new(1, :pink, 99.99, items)
+bike = Bike.new(1, :pink, 99.99)
+luggage = Luggage.new(Luggage::DEFAULT_MAX_CAPACITY, items)
 
-rental = Rental.new(bike)
+rental = Rental.new(bike, items)
 
 puts "Total Price: #{rental.price}, Total Weight: #{rental.weight}"
-
--- rental.rb
+++ rental.rb
@@ -1,17 +1,19 @@
 class Rental
 
   attr_reader :bike
+  attr_reader :luggage
 
-  def initialize(bike)
+  def initialize(bike, luggage)
     @bike = bike
+    @luggage = luggage
   end
 
   def price
-    self.bike.price + self.bike.luggage.items.count * 10
+    self.bike.price + self.luggage.count * 10 + 2 * self.weight
   end
 
   def weight
-    self.bike.weight + self.bike.luggage.items.count
+    self.bike.weight + self.luggage.count
   end
 
 end
